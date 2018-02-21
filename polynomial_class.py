7542d9fc74a03f24d227d37f7349a5ac4142dbbd
Stoddard <srpanall@gmail.com>
2018-02-20 19:19:03 -0500

    Merging home and work branches

--- polynomial_class.py
+++ polynomial_class.py
@@ -1,4 +1,6 @@
 import re
+import unittest
+
 
 poly_re = r'([+-]?\d?[A-Za-z]\^\d+)|([+-]?\d?[A-Za-z])|([+-]?\d?)'
 
@@ -7,10 +9,15 @@ term_reg = re.compile(r'[A-Za-z]\^')
 
 
 def string_to_poly(poly):
-    '''Takes a polynomial string on the form ax^n + bx^m + ... c and returns
+    '''
+    Takes a polynomial string on the form ax^n + bx^m + ... c and returns
     a Polynomial object
     '''
+    if type(poly) is not str:
+        raise TypeError
     poly_in = poly.replace(' ', '')
+    if poly_in == '':
+        raise TypeError
     var = [x for x in list(poly_in) if x.isalpha()][0]
     terms = [x for x in poly_reg.split(poly_in) if x is not None and x != '']
     terms = [[deg, coeff] for coeff, deg in [term_split(x) for x in terms]]
@@ -20,7 +27,8 @@ def string_to_poly(poly):
 
 
 def term_split(term):
-    '''takes a monomial match object and returns a tuple of
+    '''
+    takes a monomial match object and returns a tuple of
     the form (coeff, deg)
     '''
     if term[0] in ["+", "-"] and not term[1].isdigit():
@@ -87,30 +95,71 @@ class Polynomial():
         return ''.join([self.term_formater(coeff, deg) for coeff, deg in
                         term_list]).lstrip('+')
 
+    def eval_at_n(self, n):
+        '''Evaluates a polynomial at n and returns a number'''
+        return sum([coeff * n ** deg for deg, coeff in self.terms.items()])
 
-# poly = "-5x^2+3x+2"
-# p1 = string_to_poly(poly)
+    def derivitive(self):
+        '''Returns the derivative of the polynomial'''
+        new_terms = {n - 1: n * self.terms.get(n, 0) for n
+                     in range(1, self.degree + 1)}
+        return Polynomial(self.variable, new_terms)
 
-# poly2 = "6x^4-5x^2+3x+2+4x-3"
-# p2 = string_to_poly(poly2)
-
-# print(p1, "+", p2, "=", p1 + p2)
-# print(p1, "-", p2, "=", p1 - p2)
-# print(p1, "*", p2, "=", p1 * p2)
-
-# pt1 = string_to_poly("x+1")
-# pt2 = string_to_poly("x-1")
-
-# pds = pt1 * pt2
-
-# print(pds)
+    def __eq__(self, other):
+        if self.variable != other.variable:
+            return False
+        for deg in range(max(self.degree, other.degree)):
+            if self.terms.get(deg, 0) != other.terms.get(deg, 0):
+                return False
+        else:
+            return True
 
 
-# pds_t_2 = pds * 2
-# print(pds_t_2)
+# # poly = "-5x^2+3x+2"
+# # p1 = string_to_poly(poly)
 
-p1 = string_to_poly("x+4")
-p2 = string_to_poly("2x^2-3x+5")
+# # poly2 = "6x^4-5x^2+3x+2+4x-3"
+# # p2 = string_to_poly(poly2)
 
-print(p1*p2)
+# # print(p1, "+", p2, "=", p1 + p2)
+# # print(p1, "-", p2, "=", p1 - p2)
+# # print(p1, "*", p2, "=", p1 * p2)
 
+# # pt1 = string_to_poly("x+1")
+# # pt2 = string_to_poly("x-1")
+
+# # pds = pt1 * pt2
+
+# # print(pds)
+
+
+# # pds_t_2 = pds * 2
+# # print(pds_t_2)
+
+# p1 = string_to_poly("x+4")
+# p2 = string_to_poly("2x^2-3x+5")
+
+# print(p1 * p2)
+
+# print(p1.eval_at_n(5))
+# print(p2.derivitive())
+
+
+class PolynomialTests(unittest.TestCase):
+
+    def test_str_to_poly(self):
+        pn = Polynomial('x', {1: 4, 0: -3})
+        self.assertEqual(string_to_poly('4x-3'), pn)
+
+    def test_mul(self):
+        p1 = Polynomial('x', {1: 1, 0: 1})
+        p2 = Polynomial('x', {2: 1, 1: 2, 0: 1})
+        self.assertEqual(p1 * p1, p2)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
